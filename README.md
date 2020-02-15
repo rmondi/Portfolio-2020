39503c2ebc9675dd84956beb156db49d7acf808f
Rémy Mondi <remy@mondi.dev>
2020-02-15 19:19:02 +0100

    First version

---
+++ README.md
@@ -0,0 +1,9 @@
+## REQUIRE
+1. Go to the root of the project
+2. Init local configuration file : `git submodule init`
+3. Update submodules : `git submodule update`
+4. Go to gulp directory
+5. Follow installation instructions (except step 2.2) : `https://github.com/rmondi/gulp`
+
+## INSTALLATION
+1. `cp index-default.html index.html`
---
+++ src/assets/js/slider.js
@@ -0,0 +1,60 @@
+const slider = new Flickity(document.getElementById('slider'), {
+    fade: true,
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: 5000,
+    prevNextButtons: false,
+    pageDots: false
+});
+
+const body = document.querySelector('body'),
+      colors = [
+    {
+        'bgColor': 'js-background-color-1',
+        'textColor': 'js-text-color-1'
+    },
+    {
+        'bgColor': 'js-background-color-2',
+        'textColor': 'js-text-color-2'
+    },
+    {
+        'bgColor': 'js-background-color-3',
+        'textColor': 'js-text-color-1'
+    },
+    {
+        'bgColor': 'js-background-color-4',
+        'textColor': 'js-text-color-2'
+    },
+];
+
+let previousColor = 0,
+    currentColor = 1;
+
+slider.on('select', index => {
+    body.classList.remove(colors[previousColor].bgColor);
+    body.classList.remove(colors[previousColor].textColor);
+    body.classList.add(colors[currentColor].bgColor);
+    body.classList.add(colors[currentColor].textColor);
+});
+
+slider.on('change', index => {
+    if (currentColor !== colors.length -1) {
+        currentColor += 1;
+    } else {
+        currentColor = 0;
+    }
+
+    if (previousColor !== colors.length - 1) {
+        previousColor += 1;
+    } else {
+        previousColor = 0;
+    }
+});
+
+document.getElementById('previous').addEventListener('click', () => {
+    slider.previous();
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    slider.next();
+});
